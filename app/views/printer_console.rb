e60062a12a7084f4fca28b2ce7f50f6ee6470692
Manuela García <dyg9812@gmail.com>
2018-10-20 09:30:27 -0500

    cambio

--- app/views/printer_console.rb
+++ app/views/printer_console.rb
@@ -1,12 +1,12 @@
 class PrinterConsole
-  def initialize(simulator)
-    @simulator = simulator
-    @market = simulator.market
+  def initialize( queue_simulator)
+    @queue_simulator = queue_simulator
+    @market = queue_simulator.market
   end
 
   def print_prom
-    puts "el tiempo promedio de espera de los clientes fue de: #{@simulator.calculation_time_wait_clients} minutos"
-    puts "se atendieron #{@simulator.market.clients_attended.size} clientes"
+    puts "el tiempo promedio de espera de los clientes fue de: #{@queue_simulator.calculation_time_wait_clients} minutos"
+    puts "se atendieron #{@queue_simulator.market.clients_attended.size} clientes"
   end
 
   def print_sml
@@ -31,7 +31,7 @@ class PrinterConsole
   def print_queues
     (0..longest_queue_size - 1).each do |i|
       @market.queues.each do |queue|
-        print '    ' if @simulator.type_simulation == :M_1
+        print '    ' if @queue_simulator.type_simulation == :M_1
         if queue.clients[i].nil?
           print '|  | '
         else
